a24c7e3d01076c7023d38a2c66f090563e76ac32
franco.sosa <franco.sosa@conjurer.com>
2024-04-09 14:39:13 +0200

    prueba docker con traefik

--- migrations/Version20240408102822.php
+++ migrations/Version20240408102822.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240330210040 extends AbstractMigration
+final class Version20240408102822 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,17 +20,12 @@ final class Version20240330210040 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE padel_game ADD mode VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE padel_game ADD winner VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE padel_game DROP points');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE padel_game ADD points INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE padel_game DROP mode');
-        $this->addSql('ALTER TABLE padel_game DROP winner');
     }
 }
